74bf90014d35c55cca3f5be187056a4429c1ce78
zhangdaiscott <zhangdaiscott@163.com>
2022-11-14 20:18:40 +0800

    修复升级vite3导致主题设置无效问题【issues/236】【插件@rys-fe/vite-plugin-theme非官方版，但可以用，后期跟踪vben官方是否更新】
    package.json
    src\logics\theme\index.ts
    src\logics\theme\dark.ts
    build\vite\plugin\theme.ts

--- vite.config.ts
+++ vite.config.ts
@@ -82,10 +82,6 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
       // Suppress warning
       __INTLIFY_PROD_DEVTOOLS__: false,
       __APP_INFO__: JSON.stringify(__APP_INFO__),
-      //新增以下变量，临时解决 https://github.com/vbenjs/vite-plugin-theme/issues/27
-      __COLOR_PLUGIN_OUTPUT_FILE_NAME__: undefined,
-      __PROD__: true,
-      __COLOR_PLUGIN_OPTIONS__: {},
     },
     css: {
       preprocessorOptions: {
