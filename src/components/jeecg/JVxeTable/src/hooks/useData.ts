41e48c5113d0fc443d5dd7f015f93c849fa9e426
zhangdaiscott <zhangdaiscott@163.com>
2023-04-12 21:44:50 +0800

    【issues/209】VXETable自带的tooltip会错位，所以替换成原生的title

--- src/components/jeecg/JVxeTable/src/hooks/useData.ts
+++ src/components/jeecg/JVxeTable/src/hooks/useData.ts
@@ -17,10 +17,14 @@ export function useData(props: JVxeTableProps): JVxeDataProps {
       rowId: props.rowKey,
       // 高亮hover的行
       highlightHoverRow: true,
+
+      // --- 【issues/209】自带的tooltip会错位，所以替换成原生的title ---
       // 溢出隐藏并显示tooltip
-      showOverflow: true,
+      showOverflow: "title",
       // 表头溢出隐藏并显示tooltip
-      showHeaderOverflow: true,
+      showHeaderOverflow: "title",
+      // --- 【issues/209】自带的tooltip会错位，所以替换成原生的title ---
+
       showFooterOverflow: true,
       // 可编辑配置
       editConfig: {
