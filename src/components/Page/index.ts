249a66c98f1640c7e7923044ec495fa6ab6ddab1
zhangdaiscott <zhangdaiscott@163.com>
2023-05-05 11:34:21 +0800

    ReferenceError: Cannot access 'pageWrapper' before initialization #411--

--- src/components/Page/index.ts
+++ src/components/Page/index.ts
@@ -4,6 +4,7 @@ import pageFooter from './src/PageFooter.vue';
 import pageWrapper from './src/PageWrapper.vue';
 
 export const PageFooter = withInstall(pageFooter);
-export const PageWrapper = withInstall(pageWrapper);
+//【issues/411】ReferenceError: Cannot access 'pageWrapper' before initialization
+export const PageWrapper = setTimeout(()=>withInstall(pageWrapper));
 
 export const PageWrapperFixedHeightKey = 'PageWrapperFixedHeight';
