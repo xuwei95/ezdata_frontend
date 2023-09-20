da5fbb5ccd6be94f9d057bc05721f1a694fa7133
zhangdaiscott <zhangdaiscott@163.com>
2023-09-20 10:19:42 +0800

    只是修改注释

--- types/global.d.ts
+++ types/global.d.ts
@@ -52,12 +52,12 @@ declare global {
   declare interface ViteEnv {
     VITE_PORT: number;
     VITE_USE_MOCK: boolean;
-    VITE_USE_PWA: boolean;
     VITE_PUBLIC_PATH: string;
     VITE_PROXY: [string, string][];
     VITE_GLOB_APP_TITLE: string;
     VITE_GLOB_APP_SHORT_NAME: string;
     VITE_USE_CDN: boolean;
+    //20230720 作废参数
     VITE_DROP_CONSOLE: boolean;
     VITE_BUILD_COMPRESS: 'gzip' | 'brotli' | 'none';
     VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE: boolean;
